8abf49759c6dce012bb8400a7d541ddc13da6c90
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-03-24 19:47:55 +0100

    Add Mathisfun link to comment

--- add.go
+++ add.go
@@ -5,6 +5,9 @@ package demo_lib
 // Add adds two numbers and returns the result.
 // It takes in two int parameters a and b and return
 // their result.
+// [Mathisfun]
+//
+// [Mathisfun]: https://www.mathsisfun.com/numbers/addition.html.
 func Add(a, b int) int {
 	return a + b
 }
