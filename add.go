ff3d07f74b58f01359bd5918a6a8ee2cc6d75d3a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-03-24 19:28:49 +0100

    Add godoc comments to add.go

--- add.go
+++ add.go
@@ -1,5 +1,10 @@
+// package demo_lib provides basic functions to perform
+// addition and subtraction on two numbers.
 package demo_lib
 
+// Add adds two numbers and returns the result.
+// It takes in two int parameters a and b and return
+// their result.
 func Add(a, b int) int {
 	return a + b
 }
